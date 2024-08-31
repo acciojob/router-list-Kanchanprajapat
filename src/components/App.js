10f81375096a5c82568fc551cbac476ef67a553e
kanchan <kp4307699@gmail.com>
2024-08-31 20:35:27 +0530

    cpmmit

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,21 @@
-
 import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ItemList from "./ItemList";
+import ItemDetail from "./ItemDetail";
 import './../styles/App.css';
 
 const App = () => {
   return (
     <div>
-        {/* Do not remove the main div */}
+      {/* Do not remove the main div */}
+      <Router>
+        <Routes>
+          <Route path="/" element={<ItemList />} />
+          <Route path="/item/:id" element={<ItemDetail />} />
+        </Routes>
+      </Router>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
